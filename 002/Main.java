b4b59a7c782129b6f1a13c690bcf7384c8e024f7
KevinAp-5 <keven.clash3@gmail.com>
2020-12-08 20:27:58 -0300

    breakline

--- 002/Main.java
+++ 002/Main.java
@@ -3,7 +3,7 @@ class Main{
     public static void main(String[] args) {  
         Scanner sc= new Scanner(System.in);
         System.out.print("Seu nome: ");  
-        String str= sc.next();
-        System.out.print("Olá, "+str); 
+        String nome= sc.next();
+        System.out.print("Olá, "+nome+"\n");
     }
 }
