d40bf6af823f0c321ceb1fa733ea77618c4d6ef3
KevinAp-5 <keven.clash3@gmail.com>
2020-12-14 23:50:05 -0300

    015

---
+++ 015/Main.java
@@ -0,0 +1,21 @@
+/*Aluguel de carros:
+ Escreva um programa que pergunte a quantidade de Km percorridos por um carro
+ alugado e a quantidade de dias pelos quais ele foi alugado
+
+Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0.15 por
+km rodado*/
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Quantidade de km rodados: ");
+        float rodado = x.nextFloat();
+
+        System.out.print("Dias com o carro: ");
+        int dias = x.nextInt();
+        String price = String.format("%.2f", (dias * 60) + (rodado * 0.15));
+        System.out.println("Preço total: R$"+price);
+    }
+}
